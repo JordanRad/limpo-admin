5c9f4ae1f93374a0f642817fdeb87145db1e0c17
Konstantin Hadjiyankov <k.hadjiyankov@student.fontys.nl>
2021-05-17 13:21:06 +0200

    Material UI added, SVG Icons added, Axios added, Folder restructure

--- limpo-admin/src/components/App.js
+++ limpo-admin/src/components/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import '../css/App.css';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button  variant="contained" color="primary">
+        Hello
+        </Button>
         <a
           className="App-link"
           href="https://reactjs.org"
