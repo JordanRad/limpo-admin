c4fa51d93dd12db4cbd8316d0e276efb99ae38e1
Jordan Radushev <jordanradushev@Jordans-MacBook-Pro.local>
2021-07-15 18:33:30 +0300

    navbar added

--- limpo-admin/src/App.jsx
+++ limpo-admin/src/App.jsx
@@ -7,10 +7,24 @@ import {
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './config/theme-config.js';
 import LogIn from './pages/LogIn'
+import Navigation from "./components/Navigation.jsx";
+import { AppBar,Toolbar,IconButton,Typography,Button } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu'
+import React , {useState} from 'react';
 
+
+const getUser = ()=>{
+  return JSON.parse(localStorage.getItem("user"));
+}
 function App() {
+  const [drawerOpen,setDrawerOpen]=useState(false)
+  let user = getUser()
+
+
+  const [isLoggedIn, setIsLoggedIn] = useState(user!=null)
   return (
     <ThemeProvider theme={theme}>
+      
       <Router>
         <Switch>
           <Route path="/login">
@@ -30,7 +44,26 @@ function App() {
           </Route>
         </Switch>
 
-      </Router>
+      
+      <Navigation drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen}/>
+      <AppBar style={{display: !isLoggedIn?"none":"block"}}>
+    <Toolbar  >
+    <IconButton edge="start" 
+    //className={classes.menuButton} 
+    color="inherit" aria-label="menu" onClick={(e)=>{setDrawerOpen(true)}}>
+     <MenuIcon/>
+    </IconButton>
+    <Typography 
+    //className={classes.title} 
+    variant="h6" noWrap>
+            Limpo Admin
+          </Typography>
+  <Button color="inherit"></Button>
+    </Toolbar>
+  
+      
+    </AppBar>
+    </Router>
     </ThemeProvider>
   );
 }
