21830501b5c49fc4c7d60e87bff7c1bed3c9aee9
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-07-15 17:42:40 +0200

    minor change

--- limpo-admin/src/App.jsx
+++ limpo-admin/src/App.jsx
@@ -8,25 +8,25 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './config/theme-config.js';
 import LogIn from './pages/LogIn'
 import Navigation from "./components/Navigation.jsx";
-import { AppBar,Toolbar,IconButton,Typography,Button } from "@material-ui/core";
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu'
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 
-
-const getUser = ()=>{
+const getUser = () => {
   return JSON.parse(localStorage.getItem("user"));
 }
+
 function App() {
-  const [drawerOpen,setDrawerOpen]=useState(false)
   let user = getUser()
 
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(user != null)
 
-  const [isLoggedIn, setIsLoggedIn] = useState(user!=null)
   return (
     <ThemeProvider theme={theme}>
-      
       <Router>
         <Switch>
+
           <Route path="/login">
             <LogIn />
           </Route>
@@ -42,28 +42,25 @@ function App() {
           <Route path="/*">
             <Redirect to="/login" />
           </Route>
-        </Switch>
 
-      
-      <Navigation drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen}/>
-      <AppBar style={{display: !isLoggedIn?"none":"block"}}>
-    <Toolbar  >
-    <IconButton edge="start" 
-    //className={classes.menuButton} 
-    color="inherit" aria-label="menu" onClick={(e)=>{setDrawerOpen(true)}}>
-     <MenuIcon/>
-    </IconButton>
-    <Typography 
-    //className={classes.title} 
-    variant="h6" noWrap>
-            Limpo Admin v1.0
-          </Typography>
-  <Button color="inherit"></Button>
-    </Toolbar>
-  
-      
-    </AppBar>
-    </Router>
+        </Switch>
+        <Navigation drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} />
+        <AppBar style={{ display: !isLoggedIn ? "none" : "block" }}>
+          <Toolbar  >
+            <IconButton edge="start"
+              //className={classes.menuButton} 
+              color="inherit" aria-label="menu" onClick={(e) => { setDrawerOpen(true) }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              //className={classes.title} 
+              variant="h6" noWrap>
+              Limpo Admin v1.0
+            </Typography>
+            <Button color="inherit"></Button>
+          </Toolbar>
+        </AppBar>
+      </Router>
     </ThemeProvider>
   );
 }
